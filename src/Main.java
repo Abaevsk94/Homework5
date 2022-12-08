d7d8d1de93b01f0d5f597f2ba3563ff22dc67ed7
Abaevsk94 <abaevsk@gmail.com>
2022-12-08 09:26:33 +0300

    Две задачи

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,36 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        byte clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else  if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        byte clientOS = 1;
+        int clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear < clientDeviceYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= clientDeviceYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < clientDeviceYear ) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= clientDeviceYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+    static public void task3() {
+        System.out.println("Задача 3");
+
     }
 }
